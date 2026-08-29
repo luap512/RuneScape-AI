906ee588b8380c96af167fd212616c19971c3102
luap512 <phperez.work1@gmail.com>
2026-08-29 12:26:04 -0500

    Add Spring Boot project skeleton with Maven, Spring Web, Data JPA, PostgreSQL driver

---
+++ src/main/java/com/paulperez/RuneScape_AI/RuneScapeAiApplication.java
@@ -0,0 +1,13 @@
+package com.paulperez.RuneScape_AI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuneScapeAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuneScapeAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paulperez/RuneScape_AI/RuneScapeAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paulperez.RuneScape_AI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuneScapeAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
